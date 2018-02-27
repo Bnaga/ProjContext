c88ece143bd87d3d51753eb4ee097f8e3a751ea5
Robbert <bnaga93@gmail.com>
2018-02-27 22:59:02 +0100

    test1

---
+++ Assets/Scripts/RunnerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunnerScript : MonoBehaviour {
+
+    // Use this for initialization
+    private Rigidbody rbody;
+    private float runSpeed = 15;
+    public int currentClass = 0;
+	void Start ()
+    {
+        rbody = this.gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        rbody.AddForce(new Vector3(runSpeed, 0, 0));
+        //rbody.velocity = new Vector3(runSpeed, 0, 0);
+        if (currentClass == 1)
+        {
+            runSpeed = 10;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+
+        }
+}
+	}
+}
---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+    public RunnerScript runscpt;
+	// Use this for initialization
+	void Start () {
+        runscpt = this.gameObject.GetComponent<RunnerScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "class1")
+        {
+            runscpt.currentClass = 1;
+        }
+    }
+}
