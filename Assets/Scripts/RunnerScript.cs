ff42458f7c01d675629b496061feae06c14fc792
Robbert <bnaga93@gmail.com>
2018-02-28 12:22:58 +0100

    jumping

--- Assets/Scripts/RunnerScript.cs
+++ Assets/Scripts/RunnerScript.cs
@@ -6,30 +6,49 @@ public class RunnerScript : MonoBehaviour {
 
     // Use this for initialization
     private Rigidbody rbody;
-    private float runSpeed = 7;
+    private float runSpeed = 15;
     public int currentClass = 0;
-    public float jumpSpeed = 45;
+    private float jumpSpeed = 10;
+    private bool grounded = true;
 	void Start ()
     {
         rbody = this.gameObject.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void FixedUpdate ()
     {
         //rbody.AddForce(new Vector3(runSpeed, 0, 0));
-        rbody.velocity = new Vector3(runSpeed, 0, 0);
+        
         if (currentClass == 1)
         {
-            runSpeed = 5;
+            runSpeed = 10;
         }
 
         if (Input.GetButton("Jump"))
         {
             Debug.Log("jump");
             rbody.velocity = new Vector3(runSpeed, jumpSpeed, 0);
+            grounded = false;
             //rbody.AddForce(new Vector3(50, jumpSpeed, 0));
         }
+        if(!grounded)
+        {
+            Vector3 extraGravityForce = (Physics.gravity * 1.5f) - Physics.gravity;
+            rbody.AddForce(extraGravityForce);
+        }
+        else
+        {
+            rbody.velocity = new Vector3(runSpeed, 0, 0);
+        }
 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "terrain" )
+        {
+            grounded = true;
+        }
     }
 }
