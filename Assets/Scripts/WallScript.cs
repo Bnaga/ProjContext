8668a8a5e64061f9b4cf34663f3f818b9950b179
Robbert <bnaga93@gmail.com>
2018-03-06 14:12:45 +0100

    firewall will go right.

---
+++ Assets/Scripts/WallScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallScript : MonoBehaviour {
+
+    public Rigidbody rbody;
+    public float wallSpeed = 5;
+	// Use this for initialization
+	void Start () {
+        rbody = this.gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        rbody.velocity = new Vector3(wallSpeed, 0, 0);
+	}
+}
