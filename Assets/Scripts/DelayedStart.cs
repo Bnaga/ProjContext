023f63ddc8af51977e0bfd76755787ca3c4e69bf
Dirk de Meij <dirkdemeij@gmail.com>
2018-03-27 23:32:05 +0200

    CountDown

---
+++ Assets/Scripts/DelayedStart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayedStart : MonoBehaviour {
+    public GameObject CountDown;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("StartDelay");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    IEnumerator StartDelay()
+    {
+        Time.timeScale = 0;
+        float PauseTime = Time.realtimeSinceStartup + 3f;
+        while (Time.realtimeSinceStartup < PauseTime)
+            yield return 0;
+        CountDown.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+    
+
+}
